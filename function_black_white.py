21415608252651107c87accc0f020e6aa237174c
Brian <58400498+nairb725@users.noreply.github.com>
2020-12-16 14:42:16 +0100

    Update function_black_white.py
    syntaxe

--- function_black_white.py
+++ function_black_white.py
@@ -10,11 +10,10 @@ def function_black_and_white():
     print("----------FOR CHESHIRE----------")
     if os.path.isfile('Cheshire.png'):
         print("File exist")
-        # BLACK AND WHITE
+        # BLACK AND WHITE CHESHIRE
         image_cheshire = cv2.imread('Cheshire.png')
         image_gray_cheshire = cv2.cvtColor(image_cheshire, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/Cheshire_gray.png',
-                    image_gray_cheshire)
+        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/Cheshire_gray.png',image_gray_cheshire)
         print("Your image is now in black and white and you add it in a filesystem\n")
         cv2.waitKey(0)
     else:
@@ -26,11 +25,10 @@ def function_black_and_white():
     print("----------FOR SHIBA----------")
     if os.path.isfile('chien.jpg'):
         print("File exist")
-        # BLACK AND WHITE
+        # BLACK AND WHITE SHIBA
         image_shiba = cv2.imread('chien.jpg')
         image_gray_shiba = cv2.cvtColor(image_shiba, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/shiba_gray.png',
-                    image_gray_shiba)
+        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/shiba_gray.png',image_gray_shiba)
         print("Your image is now in black and white and you add it in a filesystem\n")
         cv2.waitKey(0)
     else:
@@ -42,13 +40,11 @@ def function_black_and_white():
     print("----------FOR GRUMPY CAT----------")
     if os.path.isfile('grumpy_cat_project.jpg'):
         print("File exist")
-        # BLACK AND WHITE
+        # BLACK AND WHITE GRUMPY CAT
         image_grumpy_cat = cv2.imread('grumpy_cat_project.jpg')
         image_gray_grumpy_cat = cv2.cvtColor(image_grumpy_cat, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/gray_grumpy_cat.jpg',
-                    image_gray_grumpy_cat)
+        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/gray_grumpy_cat.jpg',image_gray_grumpy_cat)
         print("Your image is now in black and white and you add it in a filesystem\n")
         cv2.waitKey(0)
     else:
         print("File doesn't exist")
-
