46bcc934b4a82236265d16828aade0690a0e147c
Cloclo78-del <72792712+Cloclo78-del@users.noreply.github.com>
2020-12-17 17:22:10 +0100

    Update Logger_filters.py

--- Logger_filters.py
+++ Logger_filters.py
@@ -1,19 +1,15 @@
 from _datetime import datetime
-log_file = 'main.log'
+log_file = 'menu.log'
 
 
 def log(msg):
+    """
+    Save a message in a log file {log_file} and prints it in the console
+    :param msg: the message to be append in the log file
+    """
     now = datetime.now()
     timestamp = now.strftime('%Y/%m/%d %H:M%:%S')
     formatted= f'{timestamp} - {msg}'
     with open(log_file, 'a') as f:
         f.write(formatted + '\n')
 
-def dump_log():
-
-    try:
-        with open(log_file, 'r') as f:
-            content = f.read()
-            print(content)
-    except FileNotFoundError as error:
-        print(error)
