7270b4ba304cabe80d557aca4520817329b879eb
Brian <58400498+nairb725@users.noreply.github.com>
2020-12-17 09:06:40 +0100

    Add files via upload
    cli du prof

--- main.py
+++ main.py
@@ -1,54 +1,23 @@
-import os.path
+import os
 import cv2
-import numpy as np
-import function_dilated
+from filters import grayscale, blur
 
-kernel = np.ones((5, 5), np.uint8)
+input_dir = 'data/imgs'
+files = os.listdir(input_dir)
+print(files)
+for f in files:
+    file_path = f"{input_dir}/{f}"
+    # 1. Ouvrir le fichier => extraire l'image qui se trouve dedans
+    # image => c'est l'image de base NON filtrée
+    image = cv2.imread(file_path)
 
-print("----------FOR CHESHIRE----------")
-if os.path.isfile('Cheshire.png'):
-    print("File exist")
-    # BLACK AND WHITE
-    image_cheshire = cv2.imread('Cheshire.png')
-    image_gray_cheshire = cv2.cvtColor(image_cheshire, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('Cheshire_gray.png', image_gray_cheshire)
-    print("Your image is now in black and white and you add it in a filesystem")
-    # DILATED
-    function_dilated.function_dilated()
-else:
-    print("File doesn't exist")
+    # 2. Applique mon filtre
+    # En input => j'ai l'image brute
+    # En résultat => j'ai l'image en N&B
+    image = grayscale.filter_grayscale(image)
+    # En input => j'ai l'image en N&B
+    # En résultat => j'ai l'image en N&B + floutée
+    image = blur.filter_blur(image)
 
-print("----------FOR SHIBA----------")
-if os.path.isfile('chien.jpg'):
-    print("File exist")
-    # BLACK AND WHITE
-    image_shiba = cv2.imread('chien.jpg')
-    image_gray_shiba = cv2.cvtColor(image_shiba, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('shiba_gray.png', image_gray_shiba)
-    print("Your image is now in black and white and you add it in a filesystem")
-    cv2.waitKey(0)
-    # DILATED
-    function_dilated.function_dilated()
-else:
-    print("File doesn't exist")
-
-print("----------FOR GRUMPY CAT----------")
-if os.path.isfile('grumpy_cat_project.jpg'):
-    print("File exist")
-    # BLACK AND WHITE
-    image_grumpy_cat = cv2.imread('grumpy_cat_project.jpg')
-    image_gray_grumpy_cat = cv2.cvtColor(image_grumpy_cat, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('gray_grumpy_cat.jpg', image_gray_grumpy_cat)
-    print("Your image is now in black and white and you add it in a filesystem")
-    cv2.waitKey(0)
-    # DILATED
-    function_dilated.function_dilated()
-else:
-    print("File doesn't exist")
-
-
-
-
-
-
-
+    # 3. Enregistrer l'image filtrée dans le dossier de sortie
+    cv2.imwrite(f"data/output/{f}", image)
