af127f83da2935557a864aa74bf6ddba3e2c319f
Brian <58400498+nairb725@users.noreply.github.com>
2020-12-17 14:34:27 +0100

    Add files via upload
    Menu.py

---
+++ menu.py
@@ -0,0 +1,14 @@
+from function_dilated import function_dilated
+from function_black_white import function_black_and_white
+from function_blurry import blurry
+print("\n\n\n\n")
+print("FUNCTION DILATED WITH PICTURES\n")
+function_dilated()
+print("FUNCTION BLACK AND WHITE WITH PICTURES\n")
+function_black_and_white()
+print("FUNCTION BLURRY WITH PICTURES\n")
+blurry()
+
+
+
+
