6d13b16e156874c2f72f659cb548dd66933bbd51
Brian <58400498+nairb725@users.noreply.github.com>
2020-12-17 09:07:36 +0100

    Update function_black_white.py

--- function_black_white.py
+++ function_black_white.py
@@ -13,7 +13,7 @@ def function_black_and_white():
         # BLACK AND WHITE CHESHIRE
         image_cheshire = cv2.imread('Cheshire.png')
         image_gray_cheshire = cv2.cvtColor(image_cheshire, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/Cheshire_gray.png',image_gray_cheshire)
+        cv2.imwrite('filesystem_gray_picture/Cheshire_gray.png',image_gray_cheshire)
         print("Your image is now in black and white and you add it in a filesystem\n")
         cv2.waitKey(0)
     else:
@@ -28,7 +28,7 @@ def function_black_and_white():
         # BLACK AND WHITE SHIBA
         image_shiba = cv2.imread('chien.jpg')
         image_gray_shiba = cv2.cvtColor(image_shiba, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/shiba_gray.png',image_gray_shiba)
+        cv2.imwrite('filesystem_gray_picture/shiba_gray.png',image_gray_shiba)
         print("Your image is now in black and white and you add it in a filesystem\n")
         cv2.waitKey(0)
     else:
@@ -43,7 +43,7 @@ def function_black_and_white():
         # BLACK AND WHITE GRUMPY CAT
         image_grumpy_cat = cv2.imread('grumpy_cat_project.jpg')
         image_gray_grumpy_cat = cv2.cvtColor(image_grumpy_cat, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite('C:/Users/33663/Documents/Projet-Python-Robin/Python/filesystem_gray_picture/gray_grumpy_cat.jpg',image_gray_grumpy_cat)
+        cv2.imwrite('filesystem_gray_picture/gray_grumpy_cat.jpg',image_gray_grumpy_cat)
         print("Your image is now in black and white and you add it in a filesystem\n")
         cv2.waitKey(0)
     else:
