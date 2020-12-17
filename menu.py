278c9f046dafa339d70b2ae618f6614c5eb4fdb9
Cloclo78-del <72792712+Cloclo78-del@users.noreply.github.com>
2020-12-17 17:18:59 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -9,6 +9,23 @@ function_black_and_white()
 print("FUNCTION BLURRY WITH PICTURES\n")
 blurry()
 
+def dictionary(blurry, dilated, gray,):
+    """
+    She is used for the CLI in order to make the parameter for the application
+    :param blurry: name of the blurry filter
+    :param dilated: name of the dilated filter
+    :param gray: name of the gray filter
+    """
+
+    filters = {
+        '1': blurry,
+        '2': dilated,
+        '3': gray,
+        '4': blurry + dilated,
+        '5': blurry + gray,
+        '6': gray + dilated,
+        '7': blurry + gray + dilated
+    }
 
 
 
