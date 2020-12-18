9e5ef1f2fc4417985234c32404475a11ea050fc1
Brian <58400498+nairb725@users.noreply.github.com>
2020-12-18 10:37:22 +0100

    Update menu.py
    agrs + import

--- menu.py
+++ menu.py
@@ -3,6 +3,10 @@ import sys
 from function_dilated import function_dilated
 from function_black_white import function_black_and_white
 from function_blurry import blurry
+from function_blurry_dilated import function_blurry_dilated
+from function_black_and_white_blurry import function_black_and_white_blurry
+from function_black_and_white_dilated import function_black_and_white_dilated
+from function_all_filters import function_all
 
 
 print("\n\n\n\n")
@@ -24,4 +28,17 @@ for arg in args:
     elif arg =='-blurry':
         print("FUNCTION BLURRY WITH PICTURES\n")
         blurry()
+    elif arg == '-blurry_dilated':
+        print("FUNCTION BLURRY AND DILATED\n")
+        function_blurry_dilated()
+    elif arg =='-black_and_white_blurry':
+        print("FUNCTION BLURRY AND BLACK AND WHITE\n")
+        function_black_and_white_blurry()
+    elif arg =='-function_black_and_white_dilated':
+        print("FUNCTION DILATED AND BLACK AND WHITE")
+        function_black_and_white_dilated()
+        print("Your image is now in blurry and black and white\n")
+    elif arg =='-function_all':
+        print("ALL FILTERS")
+        function_all()
 
