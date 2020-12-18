127effe7b869df0f48497916bc8d41388b7f93b3
Brian <58400498+nairb725@users.noreply.github.com>
2020-12-18 09:48:17 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,31 +1,27 @@
+import sys
+
 from function_dilated import function_dilated
 from function_black_white import function_black_and_white
 from function_blurry import blurry
+
+
 print("\n\n\n\n")
-print("FUNCTION DILATED WITH PICTURES\n")
-function_dilated()
-print("FUNCTION BLACK AND WHITE WITH PICTURES\n")
-function_black_and_white()
-print("FUNCTION BLURRY WITH PICTURES\n")
-blurry()
 
-def dictionary(blurry, dilated, gray,):
-    """
-    She is used for the CLI in order to make the parameter for the application
-    :param blurry: name of the blurry filter
-    :param dilated: name of the dilated filter
-    :param gray: name of the gray filter
-    """
-
-    filters = {
-        '1': blurry,
-        '2': dilated,
-        '3': gray,
-        '4': blurry + dilated,
-        '5': blurry + gray,
-        '6': gray + dilated,
-        '7': blurry + gray + dilated
-    }
-
-
+"""
+   She is used for the CLI in order to make the parameter for the application
+   :param blurry: name of the blurry filter
+   :param dilated: name of the dilated filter
+   :param gray: name of the gray filter
+   """
+args = sys.argv
+for arg in args:
+    if arg == '-dilated':
+        print("FUNCTION DILATED WITH PICTURES\n")
+        function_dilated()
+    elif arg == '-blackandwhite':
+        print("FUNCTION BLACK AND WHITE WITH PICTURES\n")
+        function_black_and_white()
+    elif arg =='-blurry':
+        print("FUNCTION BLURRY WITH PICTURES\n")
+        blurry()
 
